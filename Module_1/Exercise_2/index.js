54964561d9c7434ed5cd1191860e009884284e3b
Gonzalo Tricio <gonzalotricio4@gmail.com>
2022-11-07 11:44:52 +0100

    hola

---
+++ Module_1/Exercise_2/index.js
@@ -0,0 +1,13 @@
+let nombre = "gonsalo";
+let edad = 26;
+let mayorDeEdad = true;
+let direccion = {
+    calle: "Duques de Nájera, 97, 1ºJ",
+};
+let coloresFavoritos = ["verde", "negro"];
+let lenguajesFavoritos = ["html", "css", "java"];
+let mejorLenguaje = lenguajesFavoritos[1];
+let peorLenguaje = lenguajesFavoritos[2];
+
+console.log(mejorLenguaje);
+console.log(peorLenguaje);
