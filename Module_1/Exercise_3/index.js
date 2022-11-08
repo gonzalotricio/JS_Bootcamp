addf7528155dba08d4ec6521cf4aa4f27cbcd723
Gonzalo Tricio <gonzalotricio4@gmail.com>
2022-11-08 09:46:33 +0100

    Operaciones

---
+++ Module_1/Exercise_3/index.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("dime un número"));
+let numero2 = parseInt(prompt("dime otro número"));
+
+/* console.log(numero1 + numero2); */
+/* console.log(numero1 - numero2); */
+console.log(numero1 * numero2);
+/* console.log(numero1 / numero2); */
