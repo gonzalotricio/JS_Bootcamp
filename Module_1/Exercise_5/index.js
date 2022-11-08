33f3de99608665aefabfdaf76f751a5c1c7fa65f
Gonzalo Tricio <gonzalotricio4@gmail.com>
2022-11-08 12:23:08 +0100

    Búcles con array

---
+++ Module_1/Exercise_5/index.js
@@ -0,0 +1,21 @@
+let pokemons = ["bulbasur", "lugia", "togepi", "pikachu"];
+
+console.log(pokemons[1]);
+
+console.log(pokemons.join());
+
+for (let i = 0; i < pokemons.length; i++) {
+    console.log(pokemons[i]);
+}
+
+/* pokemons.forEach((pokemon) => {
+    console.log(pokemon);
+});
+
+for (let pokemon for pokemons) {
+    console.log(pokemon);
+}
+
+for (let pokemon in pokemons) {
+    console.log(pokemon);
+} */
