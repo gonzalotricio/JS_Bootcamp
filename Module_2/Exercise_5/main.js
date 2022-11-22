650bed6cc9db1288988fb6c24cb2821d82090eb6
Gonzalo Tricio <gonzalotricio4@gmail.com>
2022-11-22 11:36:47 +0100

    hola

---
+++ Module_2/Exercise_5/main.js
@@ -0,0 +1,11 @@
+const personas = [
+    { nombre: "Juan", edad: 18 },
+
+    { nombre: "María", edad: 16 },
+
+    { nombre: "Pedro", edad: 20 },
+
+    { nombre: "Pablo", edad: 15 },
+
+    { nombre: "Laura", edad: 19 },
+];
